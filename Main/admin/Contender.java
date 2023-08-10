8a8232deade1ea2083608eae4aae070d585c5292
siddartha2024 <141029577+siddartha2024@users.noreply.github.com>
2023-08-10 09:29:03 +0530

    Update Contender.java

--- Main/admin/Contender.java
+++ Main/admin/Contender.java
@@ -5,16 +5,23 @@ public class Contender {
     public String symbol;
     public int myScore = 0;
 
-    //boolean isEligible;
     public Contender(String name, String symbol) {
         this.name = name;
         this.symbol = symbol;
         //isEligible=true;
     }
+    public String name()
+    {
+    	return name;
+    }
+    public String symbol()
+    {
+    	return symbol;
+    }
 
     public void castForMe() {
         Main.totalParticipated++;
-        //if(!isEligible) throw new Disqualified();
+        
         myScore++;
     }
 }
